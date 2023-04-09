d0c765ad894861dff7baeae0b974c97e0c227250
SvetlanaSh72 <smile_sv7@mail.ru>
2023-04-10 02:23:23 +0500

    first commit

---
+++ identichn.py
@@ -0,0 +1,9 @@
+L = ['a', 'b', 'c']
+print(id(L))
+L.append('d')
+print(id(L))
+a = 5
+b = 3+2
+a = 5
+print(id(a))
+print(id(b))
---
+++ script.py
@@ -0,0 +1,4 @@
+num = input('входные данные: numbers= ')
+num_split = num.split()
+num_joined = "\n".join(num_split)
+print(num_joined)
